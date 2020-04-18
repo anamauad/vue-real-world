61f5bce8697ae977e4aa5667c831d35f9b7f0134
Ana Paula Mauad <ana.mauad@gmail.com>
2020-04-18 12:48:43 -0300

    Input fields -> BaseInput component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <input @input="updateValue" :value="value" v-bind="$attrs" />
+    <!-- v-bind=$attrs defines that this element will inherit the component attributes -->
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false, // props will not be applied to the main div container
+  props: {
+    value: [String, Number],
+    label: String
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
